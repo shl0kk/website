83d7fa2d55e1d47b83213fedc2e2446b9798679a
Sean Murphy <smurphspam005@gmail.com>
2023-05-26 12:50:45 +1000

    Add clock

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-// I was here - Sean
 (function ($) {
   "use strict";
 
@@ -7,3 +6,21 @@
     $(".navbar-collapse").collapse("hide");
   });
 })(window.jQuery);
+
+var clockElement = document.getElementById("clock");
+var startTime = Date.now();
+
+function updateClock() {
+  var elapsedTime = Date.now() - startTime;
+  var seconds = Math.floor(elapsedTime / 1000);
+  var minutes = Math.floor(seconds / 60);
+  var hours = Math.floor(minutes / 60);
+
+  seconds %= 60;
+  minutes %= 60;
+
+  clockElement.textContent =
+    "Time on Page: " + hours + "h " + minutes + "m " + seconds + "s";
+}
+
+setInterval(updateClock, 1000);
